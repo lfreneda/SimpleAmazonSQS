8f34ccd8dce1cfd8ad512d2be96ce75505103f00
Luiz Freneda <lfreneda@gmail.com>
2015-03-20 14:39:41 -0300

    First structure

---
+++ src/SimpleAmazonSQS/AmazonQueueService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SimpleAmazonSQS
+{
+    public class AmazonQueueService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAmazonSQS _amazonSqsClient;
+        private bool? _queueExists = null;
+
+        internal AmazonQueueService(IConfiguration configuration, IAmazonSQS amazonSqsClient)
+        {
+            _configuration = configuration;
+            _amazonSqsClient = amazonSqsClient;
+        }
+
+        public AmazonQueueService(IConfiguration configuration)
+            : this(configuration, AWSClientFactory.CreateAmazonSQSClient(configuration.AccessKey, configuration.SecretKey))
+        {
+
+        }
+
+        public virtual bool QueueExists()
+        {
+            if (_queueExists.HasValue) return _queueExists.Value;
+
+            _queueExists = false;
+
+            var queues = _amazonSqsClient.ListQueues(new ListQueuesRequest());
+            if (queues != null)
+            {
+                _queueExists = queues.QueueUrls.Any(queue => queue == _configuration.QueueUrl);
+            }
+
+            return _queueExists.Value;
+        }
+
+        public virtual void Enqueue(Guid guid)
+        {
+            _amazonSqsClient.SendMessage(new SendMessageRequest
+            {
+                QueueUrl = _configuration.QueueUrl,
+                MessageBody = guid.ToString()
+            });
+        }
+
+        public IEnumerable<Guid> Dequeue(int messageCount = 1)
+        {
+            if (messageCount < 1 || messageCount > 10)
+            {
+                throw new ArgumentOutOfRangeException("messageCount", "messageCount must be between 1 and 10.");
+            }
+
+            var response = _amazonSqsClient.ReceiveMessage(new ReceiveMessageRequest
+            {
+                QueueUrl = _configuration.QueueUrl,
+                MaxNumberOfMessages = messageCount
+            });
+
+            if (response != null && response.Messages.Any())
+            {
+                foreach (var message in response.Messages)
+                {
+                    Guid guid;
+                    if (Guid.TryParse(message.Body, out guid))
+                    {
+                        yield return guid;
+                    }
+                }
+            }
+        }
+    }
+
+    public interface IConfiguration
+    {
+        string AccessKey { get; }
+        string SecretKey { get; }
+        string QueueUrl { get; }
+    }
+
+    public class CustomConfiguration : IConfiguration
+    {
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string QueueUrl { get; set; }
+    }
+}
---
+++ test/SimpleAmazonSQS.Tests/AmazonQueueServiceTests.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace SimpleAmazonSQS.Tests
+{
+    [TestFixture]
+    public class AmazonQueueServiceTests
+    {
+        private AmazonQueueService _amazonQueueService;
+        private Mock<IAmazonSQS> _fakeAmazonSqs;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeAmazonSqs = new Mock<IAmazonSQS>();
+            _amazonQueueService = new AmazonQueueService(
+                configuration: new CustomConfiguration { SecretKey = "FakeSecretKey", AccessKey = "FakeAccessKey", QueueUrl = "http://queueurl.aws.com" },
+                amazonSqsClient: _fakeAmazonSqs.Object
+            );
+        }
+
+        [Test]
+        public void QueueExists_WhenQueueDoExists_ShouldReturnTrue()
+        {
+            _fakeAmazonSqs.Setup(c => c.ListQueues(It.IsAny<ListQueuesRequest>()))
+                          .Returns(new ListQueuesResponse
+                          {
+                              QueueUrls = new List<string>
+                              {
+                                  "http://queueurl.aws.com",
+                                  "http://queueurl.aws1.com",
+                                  "http://queueurl.aws2.com"
+                              }
+                          });
+
+            Assert.IsTrue(_amazonQueueService.QueueExists());
+        }
+
+        [Test]
+        public void QueueExists_WhenQueueDoNotExists_ShouldReturnFalse()
+        {
+            _fakeAmazonSqs.Setup(c => c.ListQueues(It.IsAny<ListQueuesRequest>()))
+                      .Returns(new ListQueuesResponse
+                      {
+                          QueueUrls = new List<string>
+                              {
+                                  "http://queueurl.aws1.com",
+                                  "http://queueurl.aws2.com"
+                              }
+                      });
+
+            Assert.IsFalse(_amazonQueueService.QueueExists());
+        }
+
+        [Test]
+        public void QueueExists_IfAmazonClientReturnNull_ShouldReturnFalse()
+        {
+            _fakeAmazonSqs.Setup(c => c.ListQueues(It.IsAny<ListQueuesRequest>()))
+                          .Returns<ListQueuesResponse>(null);
+
+            Assert.IsFalse(_amazonQueueService.QueueExists());
+        }
+
+        [Test]
+        public void Enqueue_GivenAGuid_ShouldCallAmazonClientWithExpectedData()
+        {
+            var id = Guid.NewGuid();
+            var idAsString = id.ToString();
+
+            _amazonQueueService.Enqueue(id);
+
+            _fakeAmazonSqs.Verify(mock => mock.SendMessage(
+                It.Is<SendMessageRequest>(c => c.QueueUrl == "http://queueurl.aws.com" && c.MessageBody == idAsString)
+            ), Times.Once);
+        }
+
+        [Test]
+        public void Dequeue_GivenMessaCountGreaterThan10_ShouldThrowsException()
+        {
+            Action act = () => _amazonQueueService.Dequeue(messageCount: 11).ToList();
+
+            act.ShouldThrow<ArgumentOutOfRangeException>()
+               .WithMessage("messageCount must be between 1 and 10.\r\nParameter name: messageCount");
+        }
+
+        [Test]
+        public void Dequeue_GivenMessaCountLessThan1_ShouldThrowsException()
+        {
+            Action act = () => _amazonQueueService.Dequeue(messageCount: 0).ToList();
+
+            act.ShouldThrow<ArgumentOutOfRangeException>()
+               .WithMessage("messageCount must be between 1 and 10.\r\nParameter name: messageCount");
+        }
+
+        [Test]
+        public void Dequeue_GivenMessageCountBetween1And10_ShouldCallAmazonClientWithExpectedData()
+        {
+            var messages = _amazonQueueService.Dequeue(messageCount: 10).ToList();
+
+            _fakeAmazonSqs.Verify(
+                mock => mock.ReceiveMessage(It.Is<ReceiveMessageRequest>(p => p.QueueUrl == "http://queueurl.aws.com" && p.MaxNumberOfMessages == 10)
+            ), Times.Once);
+        }
+
+        [Test]
+        public void Dequeue_IfAmazonClientReturnNull_ShouldReturnEmptyGuidList()
+        {
+            _fakeAmazonSqs.Setup(c => c.ReceiveMessage(It.IsAny<ReceiveMessageRequest>()))
+                          .Returns<ReceiveMessageResponse>(null);
+
+            var messages = _amazonQueueService.Dequeue();
+            
+            messages.ToList().Should().BeEmpty();
+        }
+
+        [Test]
+        public void Dequeue_IfMessagesReturnedAreEmpty_ShouldReturnEmptyGuidList()
+        {
+            _fakeAmazonSqs.Setup(c => c.ReceiveMessage(It.IsAny<ReceiveMessageRequest>()))
+                          .Returns(new ReceiveMessageResponse { Messages = new List<Message>() });
+
+            var messages = _amazonQueueService.Dequeue();
+
+            messages.ToList().Should().BeEmpty();
+        }
+
+        [Test]
+        public void Dequeue_GivenMessagesReturnedFromAamazonSqs_ShouldBeParsedToGuidList()
+        {
+            _fakeAmazonSqs.Setup(c => c.ReceiveMessage(It.IsAny<ReceiveMessageRequest>()))
+                          .Returns(new ReceiveMessageResponse
+                          {
+                              Messages = new List<Message>
+                              {
+                                  new Message { Body = "9e3098ce-47c0-4610-9dbc-802a64ebd757" },
+                                  new Message { Body = "9e3098ce-47c0-4610-9dbc-802a64ebd757" },
+                                  new Message { Body = "9e3098ce-47c0-4610-9dbc-802a64ebd757" }
+                              }
+                          });
+
+            var messages = _amazonQueueService.Dequeue(messageCount: 3);
+
+            messages.ToList().Should().BeEquivalentTo(new[]
+            {
+                new Guid("9e3098ce-47c0-4610-9dbc-802a64ebd757"),
+                new Guid("9e3098ce-47c0-4610-9dbc-802a64ebd757"),
+                new Guid("9e3098ce-47c0-4610-9dbc-802a64ebd757"),
+            });
+        }
+
+        [Test]
+        public void Dequeue_GivenMessageOnQueueThatIsNotAGuid_ShouldBeIgnored()
+        {
+            _fakeAmazonSqs.Setup(c => c.ReceiveMessage(It.IsAny<ReceiveMessageRequest>()))
+                          .Returns(new ReceiveMessageResponse
+                          {
+                              Messages = new List<Message>
+                              {
+                                  new Message { Body = "9e3098ce-47c0-4610-9dbc-802a64ebd757" },
+                                  new Message { Body = "im not a guid :-) what is going to happen?" },
+                                  new Message { Body = "9e3098ce-47c0-4610-9dbc-802a64ebd757" }
+                              }
+                          });
+
+            var messages = _amazonQueueService.Dequeue(messageCount: 3);
+
+            messages.ToList().Should().BeEquivalentTo(new[]
+            {
+                new Guid("9e3098ce-47c0-4610-9dbc-802a64ebd757"),
+                new Guid("9e3098ce-47c0-4610-9dbc-802a64ebd757"),
+            });
+        }
+    }
+}
