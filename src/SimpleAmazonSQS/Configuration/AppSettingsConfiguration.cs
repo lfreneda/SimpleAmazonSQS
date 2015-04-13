64ec2195bb9bd6ee60e78bafbb635909225dd588
Luiz Freneda <lfreneda@gmail.com>
2015-04-13 11:37:19 -0300

    Framework downgrade

--- src/SimpleAmazonSQS/Configuration/AppSettingsConfiguration.cs
+++ src/SimpleAmazonSQS/Configuration/AppSettingsConfiguration.cs
@@ -4,24 +4,12 @@ namespace SimpleAmazonSQS.Configuration
 {
     public class AppSettingsConfiguration : IConfiguration
     {
-        public string AccessKey
-        {
-            get { return ConfigurationManager.AppSettings["simpleAmazonSQS.accessKey"]; }
-        }
+        public string AccessKey { get { return ConfigurationManager.AppSettings["simpleAmazonSQS.accessKey"]; } }
 
-        public string SecretKey
-        {
-            get { return ConfigurationManager.AppSettings["simpleAmazonSQS.secretKey"]; }
-        }
+        public string SecretKey { get { return ConfigurationManager.AppSettings["simpleAmazonSQS.secretKey"]; } }
 
-        public string QueueUrl
-        {
-            get { return ConfigurationManager.AppSettings["simpleAmazonSQS.queueUrl"]; }
-        }
+        public string QueueUrl { get { return ConfigurationManager.AppSettings["simpleAmazonSQS.queueUrl"]; } }
 
-        public string ServiceUrl
-        {
-            get { return ConfigurationManager.AppSettings["simpleAmazonSQS.serviceUrl"]; }
-        }
+        public string ServiceUrl { get { return ConfigurationManager.AppSettings["simpleAmazonSQS.serviceUrl"]; } }
     }
 }
